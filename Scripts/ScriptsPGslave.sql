10cdf2a17946702ddeb24de3af88c826e6da2134
Sebytev3 <hipypuff@gmail.com>
2024-11-23 03:19:42 -0500

    Se agrega pentaho y bd oracle. Se deben agregar manualmente las tablas en oracle, así como abrir pentaho

---
+++ Scripts/ScriptsPGslave.sql
@@ -0,0 +1,5 @@
+select * from tabla1
+
+
+select * from tabla2
+
--- proyecto-final/docker-compose/restart-compose-rmData.sh
+++ proyecto-final/docker-compose/restart-compose-rmData.sh
@@ -1,4 +1,5 @@
 docker compose down
 docker volume rm docker-compose_postgres_ppal_data
 docker volume rm docker-compose_postgres_slave_data
+docker volume rm docker-compose_pentaho_data_integration_data
 docker compose up --build
